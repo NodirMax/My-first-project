f44b635373ba843b8e8ff269e655243d4351d59f
NodirMax <nodirmaxnodir@gmail.com>
2024-02-16 19:08:07 +0500

    new TEMA

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world!")
-	fmt.Println("Change")
+
+	// var stroka string
+	// fmt.Scan(&stroka)
+	// stroka_1 := []rune(stroka)
+	// fmt.Println(len(stroka_1))
+	z := "Hello man "
+	fmt.Println(len(z))
+	result := strings.TrimSpace(z)
+	fmt.Println(len(result))
+
 }
